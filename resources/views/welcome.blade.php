ed0b5488b18378cc5d0a228f47e70b87f4f35c13
Booooer <tsurkan.maksim2016@gmail.com>
2023-03-29 10:27:26 +0500

    изменил формочку

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,22 +7,24 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="css/app.css">
     <script src="https://code.jquery.com/jquery-3.6.4.slim.js" integrity="sha256-dWvV84T6BhzO4vG6gWhsWVKVoa4lVmLnpBOZh/CAHU4=" crossorigin="anonymous"></script>
-    <title>Document</title>
+    <title>Обновление WB</title>
 </head>
 <body>
-    <select name="data">
-        <option value="Склад">Склад</option>
-        <option value="Реализация">Реализация</option>
-        <option value="Продажи">Продажи</option>
-        <option value="Заказы">Заказы</option>
-    </select>
-    <form class="field-api" action="{{ route('update.sales') }}" method="post">
-        @csrf
-        <p>Без fetch</p>   
-        <input type="date" name="dateFrom" placeholder="Начальная дата">
-        <input type="date" name="dateTo" placeholder="Конечная дата">        
-        <button type="submit">Отправить</button>
-    </form>
+    <div class="content">
+        <select name="data" class="switch-table-name">
+            <option value="Склад">Склад</option>
+            <option value="Реализация">Реализация</option>
+            <option value="Продажи">Продажи</option>
+            <option value="Заказы">Заказы</option>
+        </select>
+        <form class="field-api" action="{{ route('update.sales') }}" method="post">
+            @csrf
+            <p id="table-name">Без fetch</p>   
+            <input type="date" name="dateFrom" placeholder="Начальная дата" required>
+            <input type="date" name="dateTo" placeholder="Конечная дата">        
+            <button type="submit">Отправить</button>
+        </form>
+    </div>
 <script src="js/app.js"></script>    
 </body>
 </html>
